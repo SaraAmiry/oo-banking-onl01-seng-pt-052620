477908141986eb84a1d36a53be967e9fc15cc968
Sara Amiry <saraamiry24@gmail.com>
2020-07-01 17:33:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
-
+  attr_accessor :name, :balance, :status
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    
+  end
+  
+  def name_error
+    raise NameError if !name.is_a?(name)
+    @name = name
+  end 
+  
+  def deposit(amount) 
+    @balance = @balance + amount
+  end 
+  
+  def display_balance
+    "Your balance is $#{@balance}." 
+  end 
+  
+  def valid? 
+    @balance > 0 && @status =="open"
+  end 
+  
+  def close_account
+    @status = "closed"
+  end 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+ attr_accessor :sender, :receiver, :status, :amount
+ 
+ def initialize(sender, receiver, amount = 50)
+   @status = 'pending'
+   @amount = amount
+   @sender = sender 
+   @receiver = receiver  
+ end 
+ 
+ def valid?
+   @receiver.valid? && @sender.valid?
+ end 
+ 
+ def execute_transaction 
+   transaction = sender.balance 
+   
+ end 
+ 
+ def reverse_transfer 
+   
+ end 
 end
